2ff23cdb5f63acf2563bae2d92b5f8037f073157
Aryan Gupta <webworksdesigners@gmail.com>
2019-07-10 17:31:01 +0530

    Updates

---
+++ precache-manifest.a20d4eb52de970b8d7435f4819e251a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e637a9dd0d79be1f6c3b2b7788f82821",
+    "url": "/TicTacToeReact/index.html"
+  },
+  {
+    "revision": "e6938253863e7a46336d",
+    "url": "/TicTacToeReact/static/css/main.c8ef81fe.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/TicTacToeReact/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "e6938253863e7a46336d",
+    "url": "/TicTacToeReact/static/js/main.357fb819.chunk.js"
+  },
+  {
+    "revision": "aae0cc895002f715d6f0",
+    "url": "/TicTacToeReact/static/js/runtime~main.10ded8ca.js"
+  }
+]);
